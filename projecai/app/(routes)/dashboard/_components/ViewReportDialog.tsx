0c6db246776b2d1afb031d91a90b18153f1b974d
Piyush Rawat <unexpected086@gmail.com>
2025-11-16 12:19:40 +0530

    QuickFIx

--- projecai/app/(routes)/dashboard/_components/ViewReportDialog.tsx
+++ projecai/app/(routes)/dashboard/_components/ViewReportDialog.tsx
@@ -8,7 +8,7 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { SessionDetail } from "../medical-agent/[sessionId]/page";
+import { SessionDetail } from "../_components/types";
 import moment from "moment";
 
 type Props = {
